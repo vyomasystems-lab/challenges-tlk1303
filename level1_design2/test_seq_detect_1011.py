9af55670d976d7ee3328f3ccfe1761bf7d562352
tlk1303 <thilak1303@gmail.com>
2022-07-24 04:33:38 +0000

    draft

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -34,16 +34,16 @@ async def test_seq_bug1(dut):
             
             if(j >= 3):
                 
-                if(inp[j-4:j] == [1,0,1,1]):
+                if(inp[j-3:j+1] == [1,0,1,1]):
                     assert dut.seq_seen.value == 1, "Random test failed with input sequence: {A}, and output: {B}, Expected ouput = 1".format(
-                        A = inp[:j], B = dut.seq_seen.value
+                        A = inp[:j+1], B = dut.seq_seen.value
                     )
-                    cocotb.log.info(f'Input sequence = {inp[:j]}, Expected output = 1, DUT Output = {dut.seq_seen.value}')
+                    cocotb.log.info(f'Input sequence = {inp[:j+1]}, Expected output = 1, DUT Output = {dut.seq_seen.value}')
                 else:
                     assert dut.seq_seen.value == 0, "Random test failed with input sequence: {A}, and output: {B}, Expected ouput = 0".format(
-                        A = inp[:j], B = dut.seq_seen.value
+                        A = inp[:j+1], B = dut.seq_seen.value
                     )
-                    cocotb.log.info(f'Input sequence = {inp[:j]}, Expected output = 0, DUT Output = {dut.seq_seen.value}')
+                    cocotb.log.info(f'Input sequence = {inp[:j+1]}, Expected output = 0, DUT Output = {dut.seq_seen.value}')
         
         dut.reset.value = 1
         await FallingEdge(dut.clk)  
